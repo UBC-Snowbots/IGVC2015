f3c9a195e6aead0c3711e18a28043e4152f6f9c5
ecandra <edbertcandra@gmail.com>
2014-11-08 13:32:07 -0800

    removing openjuas_sdk folder

--- sb_jaus/openjaus_sdk/sdk/.build/Debug/openjaus/Action.d
+++
@@ -1,6 +0,0 @@
-Debug/openjaus/Action.o: ../src/openjaus/model/Action.cpp \
- ../include/openjaus/model/Action.h ../include/openjaus/types.h
-
-../include/openjaus/model/Action.h:
-
-../include/openjaus/types.h:
