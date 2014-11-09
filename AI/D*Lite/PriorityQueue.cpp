69a19d543e3e368bd1d817cff75c9ba99d3a4b83
jechli <jenniferchingling@gmail.com>
2014-11-08 17:28:47 -0800

    Priority queue header functions and class added.

--- AI/D*Lite/PriorityQueue.cpp
+++ AI/D*Lite/PriorityQueue.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
+#include "PriorityQueue.h"
 
 using namespace std;
+
+int main() 
+{
+  MinHeap test;
+  return 0;
+}
--- AI/D*Lite/PriorityQueue.h
+++ AI/D*Lite/PriorityQueue.h
@@ -1,18 +1,24 @@
 #pragma once
 
-
 struct key  {
-	int value;
-	int cost;
+  int value;
+  int cost;
 };
 
 class MinHeap
 {
 
 private:
-	
+// keeping track of locations and keys in different arrays for searching 
+  int location[100];
+  struct key keys[100];	// array to store the frontier; statically allocated?	
+  void Sort();	// helper fnc
+
 public:
-
-	
-
+  void Remove(int location);
+  void Insert(struct key);
+  void Update(int location, struct key key);
+  void Pop(int location);
+  int Top();	  // gets the min location
+  struct key TopKey(); // Gets the min key
 }
