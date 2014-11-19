e1a3d8d0196a18a5f14f4bd7b573d14a22a5fc0f
ecandra <edbertcandra@gmail.com>
2014-11-18 22:13:42 -0800

    moving JAUS Discovery commit to source file

--- sb_jaus/src/JAUSComponent.cpp
+++ sb_jaus/src/JAUSComponent.cpp
@@ -2,7 +2,6 @@
 #include <openjaus.h>
 #include <openjaus/core/Base.h>
 #include <openjaus/mobility.h>
-#include <openjaus/mobility/GlobalPoseSensor.h>
 
 #include "JAUSComponent.hpp"
 
@@ -12,25 +11,51 @@
 	#include <termios.h>
 #endif
 
-/*
+
 using namespace openjaus;
 using namespace std;
 
-JAUSComponent::JAUSComponent():
-	       Base()
-{
-	name = "SnowBotServer";
+openjaus::system::Timer *pingTimer;
+short pingId;
 
-	cout << "JAUSComponent created" << std::endl;
-	setName("JAUSComponent");
-}*/
+JAUSComponent::JAUSComponent():
+	       Base(),
+	       pingTimer(nullptr),		
+	       pingId(0)
+{
+	name = "SnowBots JAUS Component";
+	auto discoveryService = new openjaus::model::Service();
+	discoveryService->setName("Discovery");
+	discoveryService->setUri("urn:SnowBots:Discovery");
+	discoveryService->setVersionMajor(1);
+	discoveryService->setVersionMinor(0);
+	implements->push_back(discoveryService);
+	receive.addMessageCallback(&JAUSComponent::processEcho, this);
+
+	addSelfToSystemTree();
+}
+
+bool JAUSComponent::processEcho(openjaus::core::ReportHeartbeatPulse &echo)
+{
+	std::cout << "Response from: " << echo.getSource().toString() << ": seq=" << echo.getSequenceNumber() << std::endl;
+	return true;
+}
 
 void JAUSComponent::run()
 {
-	super::run();
-	cout << "JAUS run" << std::endl;	
+	Base::run();
+	pingTimer = new openjaus::system::Timer(TIMER_METHOD(JAUSComponent, discover), this);
+	pingTimer->setInterval(1000);	
 }
 
+void JAUSComponent::discover(openjaus::system::Timer *timer)
+{
+	openjaus::core::QueryHeartbeatPulse *pingMsg = new openjaus::core::QueryHeartbeatPulse();
+	pingMsg->setSequenceNumber(pingId);
+	broadcastToNode(pingMsg);
+	//sendTimeSec.push_back(openjaus::system::Time::getTime().inSec());
+	pingId++;
+}
 
 JAUSComponent::~JAUSComponent()	
 {
--- sb_jaus/src/JAUSComponent.hpp
+++ sb_jaus/src/JAUSComponent.hpp
@@ -10,38 +10,10 @@ private:
 	openjaus::system::Timer *pingTimer;
 	short pingId;
 public:
-	JAUSComponent():
-		pingId(0),
-		pingTimer(nullptr)
-		{
-		name = "SnowBots JAUS Component";
-		
-		auto discoveryService = new openjaus::model::Service();
-		discoveryService->setName("Discovery");
-		discoveryService->setUri("urn:SnowBots:Discovery");
-		discoveryService->setVersionMajor(1);
-		discoveryService->setVersionMinor(0);
-		implements->push_back(discoveryService);
-		
-		receive.addMessageCallback(&JAUSComponent::processEcho, this);
-		
-		addSelfToSystemTree();
-	}
-	bool processEcho(openjaus::core::ReportHeartbeatPulse &echo){
-		std::cout << "Response from: " << echo.getSource().toString() << ": seq=" << echo.getSequenceNumber() << std::endl;
-		return true;
-	}
-	
-	void run(){
-		Base::run();
-		pingTimer = new openjaus::system::Timer(TIMER_METHOD(JAUSComponent, discover), this);
-		pingTimer->setInterval(1000);
-	}
-	void discover(openjaus::system::Timer *timer){
-		openjaus::core::QueryHeartbeatPulse *pingMsg = new openjaus::core::QueryHeartbeatPulse();
-		pingMsg->setSequenceNumber(pingId);
-		broadcastToNode(pingMsg);
-		//sendTimeSec.push_back(openjaus::system::Time::getTime().inSec());
-		pingId++;
-	}
+	JAUSComponent();
+	~JAUSComponent();
+
+	bool processEcho(openjaus::core::ReportHeartbeatPulse &echo);
+	void run();
+	void discover(openjaus::system::Timer *timer);
 };
