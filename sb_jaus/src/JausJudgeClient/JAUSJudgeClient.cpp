75d55ea9e9a69aaf6c446fec60f68102ce1262a0
Finn Hackett <finn.hackett@maintool.me>
2014-11-22 22:59:22 +0000

    partial discoery working, now uses config files to set static component addresses

--- sb_jaus/src/JausJudgeClient/JAUSJudgeClient.cpp
+++ sb_jaus/src/JausJudgeClient/JAUSJudgeClient.cpp
@@ -5,37 +5,27 @@
 using namespace openjaus;
 
 bool componentIdentificationResponse=false;
+core::Base component;
 
 bool processIdentificationResponse(openjaus::core::ReportIdentification& report){
 	std::cout << "Identification received: " << report.getIdentification() << std::endl;
 	std::cout << "Reponse code: " << report.getType() << std::endl;
 	componentIdentificationResponse=true;
-	return true;
+	return component.storeIdentification(&report);
 }
 
 void printMenu()
 {
 	std::cout << "Menu:" << std::endl;
 	std::cout << "t - Print System Tree" << std::endl;
-	std::cout << "1 - Find Global Pose Sensor" << std::endl;
-	std::cout << "2 - Query Global Pose" << std::endl;
-	std::cout << "3 - Query Geomagnetic Property" << std::endl;
-	std::cout << "4 - Create Periodic Event (Report GPOS)" << std::endl;
-	std::cout << "5 - Stop Periodic Event (Report GPOS)" << std::endl;
-	std::cout << "6 - Request GPOS Control" << std::endl;
-	std::cout << "7 - Release GPOS Control" << std::endl;
-	std::cout << "8 - Set Global Pose" << std::endl;
-	std::cout << "? - Output Menu" << std::endl;
-	std::cout << "ESC - Exit Component" << std::endl;
 }
 
 int main(void)
 {
 	system::Application::setTerminalMode();
 	
-	transport::Address target(1,1,1);
+	transport::Address target(155,1,1);
 
-	core::Base component;
 	component.setName("JAUSJudgeClientSim");
 	component.addMessageCallback(processIdentificationResponse);
 	component.run();
@@ -43,7 +33,7 @@ int main(void)
 	while(!componentIdentificationResponse){
 		std::cout << "Polling identification..." << std::endl;
 		openjaus::core::QueryIdentification* idQuery = new openjaus::core::QueryIdentification();
-		//idQuery->setDestination(target);
+		idQuery->setDestination(target);
 		component.sendMessage(idQuery);
 		system::Time::sleep(5000);
 	}
