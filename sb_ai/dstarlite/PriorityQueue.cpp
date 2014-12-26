d49096cfe574afefb66ee8bd37043ff894a2c894
Jennifer Ling <jenniferchingling@gmail.com>
2014-12-26 13:32:03 -0800

    PriorityQueue tested. Need to clean up code and fix up constructor.

--- sb_ai/dstarlite/PriorityQueue.cpp
+++ sb_ai/dstarlite/PriorityQueue.cpp
@@ -152,6 +152,7 @@ void MinHeap::Insert(Key key, int location)
 }
 
 
+
 void MinHeap::Update(int location, Key key)
 {
   int index = Find(location);
@@ -245,14 +246,54 @@ Key MinHeap::TopKey()
 
 int main() 
 {
-  Key test_key;
-  test_key.value = 0;
-  test_key.cost = 10;
-  MinHeap test;
-  test.Insert(test_key, 9);
-  test_key = test.TopKey();
-  cout << "Key value: " << test_key.value << endl;
-  cout << "Key cost: " << test_key.cost << endl; 
-  cout << "Location: " << test.Pop() << endl;
+  Key test_key_1, test_key_2, test_key_3, test_key_4;
+  
+  test_key_1.value = 0;
+  test_key_1.cost = 10;
+  
+  test_key_2.value = 4;
+  test_key_2.cost = 89;
+  
+  test_key_3.value = 23;
+  test_key_3.cost = 5;
+  
+  test_key_4.value = 22;
+  test_key_4.cost = 9;
+  
+  MinHeap test_heap;
+  
+  test_heap.Insert(test_key_2, 49);
+  test_heap.Insert(test_key_1, 9);
+  test_heap.Insert(test_key_3, 42);
+
+  // Pop()
+  cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+  
+  // Pop() returns null value (-1)
+  cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+
+  test_heap.Insert(test_key_4, 7);
+  test_heap.Insert(test_key_1, 9);
+  test_heap.Insert(test_key_3, 42);
+  test_heap.Insert(test_key_2, 39);
+ 
+	test_heap.Remove(7);
+	test_heap.Remove(30);
+	
+	test_key_3.cost = 3;
+	
+	test_heap.Update(42, test_key_3);
+	
+	cout << "Top key: " << test_heap.TopKey().cost << endl;
+	
+	cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+  cout << test_heap.Pop() << endl;
+  
   return 0;
 }
