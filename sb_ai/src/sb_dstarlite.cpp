3c4401f44e15c42547b5db0949d270cb4d12b079
jechli <jenniferchingling@gmail.com>
2014-12-27 13:30:33 -0800

    AI ros node builds.

--- sb_ai/src/sb_dstarlite.cpp
+++ sb_ai/src/sb_dstarlite.cpp
@@ -6,10 +6,12 @@
 
 
 // callback to receive data from subscription
+// this is where you do what you need to do with the map data
 void map_callback(const std_msgs::String::ConstPtr& msg)
 {
-
+	ROS_INFO("I heard: [%s]", msg->data.c_str());
 }
+
 
 // main function that runs this node
 int main(int argc, char **argv)
@@ -21,11 +23,12 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, AI_NODE_NAME);
 	
 	// Main access point to communication with ROS system.
-	ros::NodeHandle ai_nh;
+	ros::NodeHandle nh;
 	
 	// Initialize publisher to PUB_TOPIC.
 	// Second argument for advertise is the buffer size.
-	ros::Publisher car_pub = nh.advertise<std_msgs::String>(PUB_TOPIC, 100);
+	ros::Publisher car_pub = nh.advertise<std_msgs::String>(PUB_TOPIC, 100);	
+	ros::Subscriber map_sub = nh.subscribe(MAP_SUB_TOPIC, 100, map_callback);
 	
 	// Frequency of the loop. 
 	// eg. 10 = 10hz
@@ -42,7 +45,7 @@ int main(int argc, char **argv)
 		
 		msg.data = ss.str();
 		
-		car_pub.publish(msg);
+		car_pub.publish(msg);	// this is where we publish our map analysis
 		
 		ros::spinOnce();	// required for subscriptions
 		
