972b7596f6b5402c9edfdceeaec597318ed66643
Dulluhan <yuanv@alumni.ubc.ca>
2015-01-03 14:06:24 +0800

    Updated gps2.cpp 1/3/2015 2:06

--- sb_gps/src/gps2.cpp
+++ sb_gps/src/gps2.cpp
@@ -1,7 +1,19 @@
-#include <iostream> 
+/* Author: Vincent Yuan
+*  Date: Jan 3, 2015
+*  Purpose: Main GPS function node
+*  Function: Collect, parse NMEA data, create twist message, r, theta
+*/ 
+
+//Standard Headers
 #include <stlib.h> 
 #include <math.h> 
 
+#include <iostream>
+#include <sstream>  
+
+
+#define TRUE 1 
+#define FALSE 0 
 
 using namepsace std; 
 
@@ -17,8 +29,42 @@ int lon_x;
 int lat_y;
 } 
 
+// Variables 
+
+
 //functions 
-int gpsStatus (); //Checking if gps has started receiving data 
-int nmeaParse ();  
+int gpsStatus (); //Checking if gps has started receiving data
+void nmeaParse (); //Parse intake data, from USB/buffer.txt/suscribe? Need to write driver
+void getWaypoint (); //collects waypoint from txt file 
+void createAngle (double *theta); //Calculates Angle  
+void createDistance (double *d); //Calculates Distance
+int checkGoal (); //Check to see if we are at destination 
+void longToMetre (); //Calculates long/lat for distance in meters
+void createTwist (); //Calculates Twist
+/* The 3 create functions should be suscribed to
+
+*/ 
+
+
+int main (void){
+	struct waypoint current = 0; 
+
+	while (gpsStatus()){
+		nmeaParse();
+	
+	} 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
