50df65762fd93cd45a7f1d90f39a9ab19cdcfa5e
Jennifer Ling <jenniferchingling@gmail.com>
2015-01-31 11:42:09 -0800

    test output for AI

--- sb_ai/src/README.txt
+++ sb_ai/src/README.txt
@@ -1,6 +1,12 @@
 All classes of algorithms are found in the AI folder.
 
+TEST / SIMULATION FILES: 
+
 map_sim.cpp: This is a node that simulates the retrieval of a map from the lidar. The output format is sb_msgs/AISimMap.msg.
+
+test_output.cpp: This is the test file that publishes to the car command topic to test the robot's reponse to a simulated AI instruction.
+
+ALGORITHMS:
 
 sb_ai.h: This is a header file of information that any of the sb_ai algorithm nodes would need, such as the name of the topics they are subscribing and publishing to.
 
---
+++ sb_ai/src/test_output.cpp
@@ -0,0 +1,44 @@
+#include "ros/ros.h"
+#include <string>
+#include <iostream>
+#include <geometry_msgs/Twist.h>
+
+using namespace std;
+
+static const string NODE_NAME = "ai_test_output";
+static const string CAR_COMMAND_TOPIC = "lidar_nav";
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, NODE_NAME);
+	ros::NodeHandle n;
+	ros::Publisher car_pub = n.advertise<geometry_msgs::Twist>(CAR_COMMAND_TOPIC, 100);
+	ros::Rate loop_rate(10);  // 10 hz
+
+	int count = 0;
+	geometry_msgs::Twist twist;
+
+	while (ros::ok()) {
+		
+		if (count < 30) {
+			twist.linear.x = 0;
+			twist.linear.y = 1;
+			twist.linear.z = 0;
+			twist.angular.x = 0;
+			twist.angular.y = 0;
+			twist.angular.z = 0;
+		}
+
+		else {
+			twist.linear.x = 1;
+			twist.linear.y = 0;
+			twist.linear.z = 0;
+			twist.angular.x = 0;
+			twist.angular.y = 0;
+			twist.angular.z = 0;
+		}
+		car_pub.publish(twist);
+		loop_rate.sleep();
+	}
+	return 0;
+}
