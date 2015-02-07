47bc6f1bef0f19a3a702a3416327d5cb9535e54b
ecandra <edbertcandra@gmail.com>
2015-02-07 12:42:48 -0800

    adding transport service

--- sb_jaus/src/judge.cpp
+++ sb_jaus/src/judge.cpp
@@ -1,23 +1,34 @@
 #include <jaus/core/Component.h>
+#include <jaus/core/transport/Transport.h>
 #include <iostream>
 
 int main(){
         JAUS::Component component;
         JAUS::Discovery* discoveryService = nullptr;
+        JAUS::Transport* transportService = nullptr;
+
         discoveryService = (JAUS::Discovery*)component.GetService(JAUS::Discovery::Name);
         
         discoveryService->SetSubsystemIdentification(JAUS::Subsystem::Vehicle,"SnowBots_JudgeSim");
         discoveryService->SetNodeIdentification("Main");
         discoveryService->SetComponentIdentification("Baseline");
         
-        JAUS::Address componentID(2000,1,1);
+        JAUS::Address componentID(9000,1,1);
         if(component.Initialize(componentID)==false){
                 std::cout << "Failed to initialize [" << componentID.ToString() << std::endl;
                 return 0;
         }
         std::cout << "Success!" << std::endl;
+
+        transportService = (JAUS::Transport*) component.GetService(JAUS::Transport::Name);
+        transportService->LoadSettings("services.xml");
+        if(!transportService->IsInitialized())
+        {
+                const JAUS::Address comp_address_id = component.GetComponentID();
+                transportService->Initialize(comp_address_id);
+        }
         
-        JAUS::QueryIdentification id_query(JAUS::Address(1000,1,1),component.GetComponentID());
+        JAUS::QueryIdentification id_query(JAUS::Address(7000,1,1),component.GetComponentID());
         JAUS::ReportIdentification id_response;
         while(!component.Send(&id_query,&id_response,5000));
         std::cout << "ID: " << id_response.GetIdentification() << std::endl;
--- sb_jaus/src/server.cpp
+++ sb_jaus/src/server.cpp
@@ -1,36 +1,54 @@
 #include <jaus/core/Component.h>
+#include <jaus/core/transport/Transport.h>
 #include <iostream>
 
-int main(){
+int main()
+{
         JAUS::Component component;
         JAUS::Discovery* discoveryService = nullptr;
+        JAUS::Transport* transportService = nullptr;
+
         discoveryService = (JAUS::Discovery*)component.GetService(JAUS::Discovery::Name);
-        
         discoveryService->SetSubsystemIdentification(JAUS::Subsystem::Vehicle,"Snowbots");
         discoveryService->SetNodeIdentification("Main");
         discoveryService->SetComponentIdentification("Baseline");
-        
-        JAUS::Address componentID(1000,1,1);
-        if(component.Initialize(componentID)==false){
-                std::cout << "Failed to initialize [" << componentID.ToString() << std::endl;
+
+        JAUS::Address componentID(7000,1,1);
+        if(component.Initialize(componentID)==false)
+        {
+                std::cout << "Failed to initialize [" << componentID.ToString() << "]" << std::endl;
                 return 0;
         }
         std::cout << "Success!" << std::endl;
+
+        transportService = (JAUS::Transport*) component.GetService(JAUS::Transport::Name);
+        transportService->LoadSettings("services.xml");
+        if(!transportService->IsInitialized())
+        {
+                const JAUS::Address comp_address_id = component.GetComponentID();
+                transportService->Initialize(comp_address_id);
+        }
+
+        JAUS::Management* managementService = nullptr;
+        managementService = (JAUS::Management*)component.GetService(JAUS::Management::Name);
         
         JAUS::Time::Stamp displayStatusTimeMs = JAUS::Time::GetUtcTimeMs();
-        while(true){
-                JAUS::Management* managementService = nullptr;
-                managementService = (JAUS::Management*)component.GetService(JAUS::Management::Name);
-                if(managementService->GetStatus() == JAUS::Management::Status::Shutdown){
+        while(true)
+        {
+                if(managementService->GetStatus() == JAUS::Management::Status::Shutdown)
+                {
                         break;
                 }
-                if(JAUS::Time::GetUtcTimeMs() - displayStatusTimeMs > 500){
+                if(JAUS::Time::GetUtcTimeMs() - displayStatusTimeMs > 500)
+                {
                         std::cout << "==================" << std::endl;
                         managementService->PrintStatus();
                         std::cout << std::endl;
                         displayStatusTimeMs = JAUS::Time::GetUtcTimeMs();
                 }
+
                 CxUtils::SleepMs(1);
+        
         }
         component.Shutdown();
         return 0;
