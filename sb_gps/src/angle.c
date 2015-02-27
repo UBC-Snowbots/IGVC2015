e9948c2380c362e04f6590eb42fd5a633b47ff93
nickwu241 <nickwu241@gmail.com>
2015-02-27 03:31:45 -0800

    edited the createAngle section of gps2.cpp, the function needs to be checked.

--- sb_gps/src/angle.c
+++ sb_gps/src/angle.c
@@ -2,60 +2,61 @@
 #include <stdio.h>
 #include <math.h>
 
-//Functions
-void getTheta1 (double compass, double phi);
-void getTheta2 (double compass, double phi);
+//Constants
+#define PI 3.14159265359
 
 int main(void){
+	//Variables
+	double theta; //angle needed to turn robot into goal direction
+	double phi; //variable needed to calculate theta
+	double x; //x cordinate of the goal
+	double y; //y cordinate of the goal
+	double r; //distance from the robot to the goal
+	double angleRobot; //angle of robot to the y-axis
+	double angleCompass; //current angle of robot from North (0-359)
 
-	double phi = 0;
-	double theta = 0;
-	double x = 1;
-	double y = 1;
-	double r = 5; 
-	double s = acos( y/r );
-
-	while ( x > 0, y >= 0 ) {
-		phi = s;
-		if ( compass <= (phi + 180) ) {
-		getTheta1 (compass, phi);
+	r = sqrt(x*x + y*y);
+	angleRobot = 180 * (acos(y / r) / PI);
+	
+	//while direction of goal angle is in quadrant 1
+	while (x > 0 && y >= 0) {
+		phi = angleRobot;
+		if (angleCompass <= (phi + 180)) {
+			theta = (phi - angleCompass);
 		}
-		else if ( compass > (phi + 180) ) {
-		getTheta2 (compass, phi);
-		}	
-	}
-	while ( x >= 0, y > 0 ) {
-		phi = (180 - s);
-		if ( compass <= (phi + 180) ) {
-		getTheta1 (compass, phi);
-		}
-		else if ( compass > (phi + 180) ) {
-		getTheta2 (compass, phi);
+		else if (angleCompass > (phi + 180)) {
+			theta = (360 - angleCompass + phi);
 		}
 	}
-	while ( x < 0, y <= 0 ) {
-		phi = (180 + s);
-		if ( compass < (phi - 180) ) {
-		getTheta1 (compass, phi);
+	//while direction of goal angle is in quadrant 4
+	while (x >= 0 && y < 0) {
+		phi = (180 - angleRobot);
+		if (angleCompass <= (phi + 180)) {
+			theta = (phi - angleCompass);
 		}
-		else if ( compass > (phi - 180) ) {
-		getTheta2 (compass, phi);
+		else if (angleCompass >(phi + 180)) {
+			theta = (360 - angleCompass + phi);
 		}
 	}
-	while ( x < 0, y > 0 ) {
-		phi = (360 - s);
-		if ( compass < (phi - 180) ) {
-		getTheta1 (compass, phi);
+	//while direction of goal angle is in quadrant 3
+	while (x < 0 && y <= 0) {
+		phi = (180 + angleRobot);
+		if (angleCompass < (phi - 180)) {
+			theta = (phi - angleCompass);
 		}
-		else if ( compass > (phi - 180) ) {
-		getTheta2 (compass, phi);
+		else if (angleCompass >(phi - 180)) {
+			theta = (360 - angleCompass + phi);
+		}
+	}
+	//while direction of goal angle is in quadrant 2
+	while (x < 0 && y > 0) {
+		phi = (360 - angleRobot);
+		if (angleCompass < (phi - 180)) {
+			theta = (phi - angleCompass);
+		}
+		else if (angleCompass >(phi - 180)) {
+			theta = (360 - angleCompass + phi);
 		}
 	}
 	return 0;
-}
-void getTheta1 (double compass, double phi) {
-	theta = (phi - compass);
-}
-void getTheta2 (double compass, double phi) {
-		theta = (360 - compass + phi);
 }
