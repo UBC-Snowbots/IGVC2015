a6aadf26c9c384adcecb9150d8b10bb1c119d6d3
Finn Hackett <finn.hackett@maintool.me>
2015-03-28 14:48:42 -0700

    added documentation to ControllerBase.hpp

--- sb_ai/src/ControllerBase.hpp
+++ sb_ai/src/ControllerBase.hpp
@@ -7,7 +7,18 @@ namespace ai{
 
 class ControllerBase{
 public:
+	/**
+	 *	Override this to do your set-up (registering handlers and such)
+	**/
+	ControllerBase(){}
+	/**
+	 *	Override this if you want to specify a specific rate at which you want update() called.
+	**/
 	virtual int get_clock_speed() { return 10; }
+	/**
+	 *	This method is called by the main loop approximately once every get_clock_speed() ms.
+	 *	Anything you want to run continuuously goes here (you should return your Twist messages so they can be published)
+	**/
 	virtual geometry_msgs::Twist update() = 0;
 	virtual ~ControllerBase(){}
 };
