f6421bf0cc0158750ce0990ac43d588a8b45fe22
jarekAIM <jarek.ignasmenzies@gmail.com>
2015-05-11 18:06:52 -0700

    Update elsa_demo_v0_1.ino
    added code for reading compass

--- elsa_demo_v0_1.ino
+++ elsa_demo_v0_1.ino
@@ -30,6 +30,7 @@
 #include <AP_Declination.h>
 
 #include <AP_BattMonitor.h>//note needs the modified libary to function
+#include <AP_Compass.h> // Compass Library
 
 #ifdef DOES_ARDUINO_NOT_SUPPORT_CUSTOM_INCLUDE_DIRECTORIES
 #include <AP_ADC_AnalogSource.h>
@@ -63,6 +64,16 @@ int safety_count=0;
 AP_HAL::DigitalSource *a_led;
 AP_HAL::DigitalSource *b_led;
 
+const AP_HAL::HAL& hal = AP_HAL_BOARD_DRIVER;//inisilizing the compass
+AP_InertialSensor_MPU6000 ins;
+
+#if CONFIG_HAL_BOARD == HAL_BOARD_PX4
+AP_Compass_PX4 compass;
+#else
+AP_Compass_HMC5843 compass;
+#endif
+uint32_t timer;
+
 void setup()
 {
   setup_radio();
@@ -91,6 +102,9 @@ void setup()
   b_led = hal.gpio->channel(53);//A9 output for LEDs
   b_led->mode(GPIO_OUTPUT);
   b_led->write(0);
+  
+  //set up compas and MPU6000
+  setup_compass();
 }
 
 void loop()
@@ -99,6 +113,7 @@ void loop()
   read_radio();
   talk();
   move_pwm();
+  run_compass();
 }
 
 void read_radio()
@@ -290,6 +305,116 @@ void talk()
       }
     }
   }
+}
+
+void setup_compass()
+{
+  compass.set_offsets(0,0,0); // set offsets to account for surrounding interference
+  compass.set_declination(ToRad(0.0)); // set local difference between magnetic north and true north
+
+  hal.scheduler->delay(1000);
+  timer = hal.scheduler->micros();
+
+#if CONFIG_HAL_BOARD == HAL_BOARD_APM2
+    // we need to stop the barometer from holding the SPI bus
+    hal.gpio->pinMode(40, GPIO_OUTPUT);
+    hal.gpio->write(40, 1);
+#endif
+
+    ins.init(AP_InertialSensor::COLD_START, 
+			 AP_InertialSensor::RATE_100HZ);
+
+    if (!compass.init()) {
+        hal.console->println("compass initialisation failed!");
+        while (1) ;
+    }
+
+    compass.set_offsets(0,0,0); // set offsets to account for surrounding interference
+    compass.set_declination(ToRad(0.0)); // set local difference between magnetic north and true north
+}
+
+void run_compass()//compass function, remove prints and consol reads
+{
+    Vector3f accel;
+    Vector3f gyro;
+    float length;
+	uint8_t counter = 0;
+    static float min[3], max[3], offset[3];
+
+    compass.accumulate();
+
+    // flush any user input
+    while( hal.console->available() ) {
+        hal.console->read();
+    }
+
+    // clear out any existing samples from ins
+    ins.update();
+
+    // loop as long as user does not press a key
+    while( !hal.console->available() ) {
+
+      
+
+ timer = hal.scheduler->micros();
+        compass.read();
+        unsigned long read_time = hal.scheduler->micros() - timer;
+        float heading;
+
+        if (!compass.healthy()) {
+            hal.console->println("not healthy");
+            return;
+        }
+	Matrix3f dcm_matrix;
+	// use roll = 0, pitch = 0 for this example
+	dcm_matrix.from_euler(0, 0, 0);
+        heading = compass.calculate_heading(dcm_matrix);
+        compass.null_offsets();
+
+        // capture min
+        const Vector3f &mag = compass.get_field();
+        if( mag.x < min[0] )
+            min[0] = mag.x;
+        if( mag.y < min[1] )
+            min[1] = mag.y;
+        if( mag.z < min[2] )
+            min[2] = mag.z;
+
+        // capture max
+        if( mag.x > max[0] )
+            max[0] = mag.x;
+        if( mag.y > max[1] )
+            max[1] = mag.y;
+        if( mag.z > max[2] )
+            max[2] = mag.z;
+
+        // calculate offsets
+        offset[0] = -(max[0]+min[0])/2;
+        offset[1] = -(max[1]+min[1])/2;
+        offset[2] = -(max[2]+min[2])/2;
+
+        // display all to user
+
+
+        hal.console->println();
+    
+
+
+        // wait until we have a sample
+        ins.wait_for_sample(read_time);
+
+        // read samples from ins
+        ins.update();
+        accel = ins.get_accel();
+        gyro = ins.get_gyro();
+
+        length = accel.length();
+
+hal.console->printf_P(PSTR("%.2f\t\t\t\t%u \t\t  %4.2f  %4.2f  %4.2f \t \t %4.2f \t\t\t%4.2f %4.2f %4.2f\n"), 
+								ToDeg(heading), (unsigned)read_time, accel.x, accel.y, accel.z, length, gyro.x, gyro.y, gyro.z);
+
+
+}
 }
 
 AP_HAL_MAIN();
