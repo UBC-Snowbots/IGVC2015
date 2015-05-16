b72b1d9a413b223b00b0a63dda9a339fb48d0ba5
Andres Rama <and-yrama@live.com>
2015-05-16 14:32:43 -0700

    FUCK YES SHIT WORKS YO

--- sb_jaus/src/judge.cpp
+++ sb_jaus/src/judge.cpp
@@ -24,60 +24,6 @@
 
 const unsigned int SUBSYSTEM_TO_TEST = 100;
 
-JAUS::Component component;
-
-void run_testing() {
-    std::cout << "jk not really - need to write this bit" << std::endl;
-}
-
-std::thread testing;
-
-void communications_loop(JAUS::Component &comp, JAUS::Message &query, JAUS::Message &response){
-  unsigned int i = 0;
-    while(!component.Send(&query,&response,5000)){
-      ++i;
-      std::cout << "...";
-      usleep(1000);
-      std::cout << "\b\b\b\033[K";
-    }
-    std::cout << "Waited: " << i*1000 << "ms" << std::endl;
-}
-
-
-class DiscoveryCallback: public JAUS::Discovery::Callback {
-public:
-    void ProcessMessage(const JAUS::Message* msg) override {
-        JAUS::Discovery::Callback::ProcessMessage(msg);
-        const JAUS::ReportIdentification* report = dynamic_cast<const JAUS::ReportIdentification*>(msg);
-        if(report) {
-            std::string type;
-            switch(report->GetType()) {
-            case JAUS::ReportIdentification::IdentificationType::Vehicle:
-                type = "Vehicle";
-                break;
-            case JAUS::ReportIdentification::IdentificationType::OtherSubsystem:
-                type = "Other Subsystem";
-                break;
-            case JAUS::ReportIdentification::IdentificationType::Node:
-                type = "Node";
-                break;
-            case JAUS::ReportIdentification::IdentificationType::Component:
-                type = "Component";
-                break;
-            default:
-                type = "Something";
-            }
-            std::cout << "Found " << type << ": " << report->GetIdentification() << std::endl;
-
-            if(report->GetSourceID().mSubsystem == SUBSYSTEM_TO_TEST) {
-                std::cout << "This is the test subsystem - start testing thread" << std::endl;
-
-                testing = std::thread(run_testing);
-            }
-        }
-    }
-};
-
 int main() {
     JAUS::Component component;
     JAUS::Discovery* discoveryService = nullptr;
@@ -85,11 +31,16 @@ int main() {
 
     discoveryService = (JAUS::Discovery*)component.GetService(JAUS::Discovery::Name);
 
-    discoveryService->EnableDebugMessages(true);
+    discoveryService->EnableDebugMessages(false);
 
     discoveryService->SetSubsystemIdentification(JAUS::Subsystem::OtherSubsystem,"SnowBots_JudgeSim");
     discoveryService->SetNodeIdentification("Main");
     discoveryService->SetComponentIdentification("Baseline");
+    
+    transportService = (JAUS::Transport*)component.GetService(JAUS::Transport::Name);
+    transportService->SetDisconnectTimeMs(10000);
+    transportService->EnableDebugMessages(true);
+    
     int comp_id = 1000;
     JAUS::Address componentID(comp_id,1,1);
     while(component.Initialize(componentID)==false) {
@@ -112,37 +63,47 @@ int main() {
         transportService->Initialize(comp_address_id);
     }
 
+    //Original, working code
+    JAUS::QueryIdentification og_id_query(JAUS::Address(5000,1,1),component.GetComponentID());
+    JAUS::ReportIdentification og_id_response;
+    while(!component.Send(&og_id_query,&og_id_response,5000));
+    std::cout << "ID: " << og_id_response.GetIdentification() << std::endl;
+
+    
     JAUS::QueryIdentification id_query(JAUS::Address(5000,1,1),component.GetComponentID());
     JAUS::ReportIdentification id_response;
-    communications_loop(component, id_query, id_response);
+    component.Send(&id_query, &id_response);
     std::cout << "ID: " << id_response.GetIdentification() << std::endl;
-    
+
+
     //Test to see if our bot can basic
     JAUS::QueryHeartbeatPulse heart_query(JAUS::Address(5000,1,1),component.GetComponentID());
     JAUS::ReportHeartbeatPulse heart_response;
-    communications_loop(component, heart_query, heart_response);
+    component.Send(&heart_query, &heart_response);
     heart_response.PrintMessageBody();
+    
 
     //Time to find out if the auth be working
+    JAUS::AccessControl* access_control = (JAUS::AccessControl*)component.GetService(JAUS::AccessControl::Name);
+    access_control->SetAuthorityCode(255);
     JAUS::QueryAuthority auth_query(JAUS::Address(5000, 1, 1), component.GetComponentID());
     JAUS::ReportAuthority auth_response;
-    communications_loop(component, auth_query, auth_response);
+    component.Send(&auth_query, &auth_response);
     auth_response.PrintMessageBody();
-    
+
     //Now to see if our bot can strike a pose
     JAUS::QueryGlobalPose global_pose_query(JAUS::Address(5000, 1, 1), component.GetComponentID());
     JAUS::ReportGlobalPose global_pose_response;
-    communications_loop(component, global_pose_query, global_pose_response);
+    component.Send(&global_pose_query, &global_pose_response);
     global_pose_response.PrintMessageBody();
-    
+
     //How about we meet the locals?
     JAUS::QueryLocalPose local_pose_query(JAUS::Address(5000, 1, 1), component.GetComponentID());
     JAUS::ReportLocalPose local_pose_response;
-    communications_loop(component, local_pose_query, local_pose_response);
+    component.Send(&local_pose_query, &local_pose_response);
     local_pose_response.PrintMessageBody();
-    
-    
-    
+
+
     //Debug still live
     component.Shutdown();
     return 0;
