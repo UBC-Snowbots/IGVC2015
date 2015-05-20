cdee0a8cda175d5cf1d9a9cdcba8e72a41e20fc8
jarekAIM <jarek.ignasmenzies@gmail.com>
2015-05-20 12:37:24 -0700

    Update arduino_driver.h
    updated header file to mach cpp

--- sb_driver/src/arduino_driver.h
+++ sb_driver/src/arduino_driver.h
@@ -20,20 +20,10 @@
 
 using namespace std;
 
-//servo control struct for servo_command
-struct ServoControl
-{
-    int16_t throttle;
-    int16_t steering;
-    int16_t pan;
-    int16_t tilt;
-};
-
 //for mechanul
 struct MechControl
 {
     int twist_y;
-    int twist_x;
     int twist_z;
 };
 void processData(string data, sb_msgs::RobotState &state);
@@ -44,11 +34,6 @@ void processData(string data, sb_msgs::RobotState &state);
 */
 void car_command_callback(const geometry_msgs::TwistConstPtr& msg_ptr);
 
-/*
- turret_command_callback function
- takes a TurretCommand message and puts data into struct to send to robot to control turret servos
-*/
-void turret_command_callback(const sb_msgs::TurretCommandConstPtr& msg_ptr);
 
 /*
  eStop_callback function
