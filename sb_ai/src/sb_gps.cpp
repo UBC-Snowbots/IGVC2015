691bd42ff7a72dda5ba5d877c31003c812cb0b0a
Dulluhan <yuanv@alumni.ubc.ca>
2015-05-21 14:04:12 -0700

    comitt before I start working today

--- sb_ai/src/sb_gps.cpp
+++ sb_ai/src/sb_gps.cpp
@@ -1,4 +1,4 @@
-//Author: Vincent Yuan 
+//Author: Vincent Yuan & Nick Wu
 //Date: May 19, 2015
 //Purpose: GPS Data processing 
 //Input: Compass Data (sb_driver node), GPS Data (sb_gps node) 
@@ -17,6 +17,7 @@
 using namespace std; 
 
 #define EARTH_RADIUS 6378.137
+#define PI 3.14159265
 
 struct waypoint {
   double lon;
@@ -24,63 +25,100 @@ struct waypoint {
   waypoint(){
     lon=lat=0;
   }
+  waypoint(double x,double y){
+	lon = x; 
+	lat = y;
+	}
 };
 double *NMEA; //To hold received suscription message
 double angleCompass;
 bool moveStatus;
 bool goal;
-bool gpsFlag = true; //indicates connection from gps chip
+bool msg_flag = false;
 double d = 0; //distance in metres from currentWaypoint to targetWaypoint
 double theta = 0; //theta is the angle the robot needs to turn from currentWaypoint to targetWaypoint
 waypoint CurrentWaypoint,TargetWaypoint,LastWaypoint;
 geometry_msgs::Twist twist_msg;
-int next_movement = 0;
+int next_move, prev_move = 0;
+waypoint off;
 
+void startGps(void){
+  double lon = 49.262511;
+  double lat = -123.248619;
+  off.lon = lon - CurrentWaypoint.lon;
+  off.lat = lat - CurrentWaypoint.lat;
+  return;
+}
 
 void gpsSubHandle(const std_msgs::String::ConstPtr& msg){
-  /*
- 	Input Parameter: Suscribed Message
- 	Output: void
- 		1. Pointer
- 	Purpose: Handles suscribed gps Message, check for NMEA, Parse
- 	Notes: Copied from last years' code
-  */
- 	char a[64];
- 	char temp[8];
- 	int x, y, i = 0, j;
- 	string str = msg->data;
-
- 	while (str[i] != '\0') { a[i] = str[i]; i++; }
- 	if (a[0] != 'B' && a[1] != ',') { cout << "no B" << endl; return; }
- 	if( a[10] != 'e' && a[24] != 'e') { cout << "no e" << endl; return; }
- 	if( a[14] != ',' && a[28] != ',') { cout << "no comma" << endl; return; }
- 	if( a[33] != '.') { cout << "no compass" << endl; return; }
-
- 	//processing x coord
- 	for (i = 2, j=0; i<9 || j<7; i++, j++) {
- 		if (a[i]=='.') { j--; }
- 		else { temp[j] = a[i]; }
- 	}
- 	LastWaypoint.lon = CurrentWaypoint.lon;
- 	CurrentWaypoint.lon = atoi(temp);
-
- 	//processing y coord
- 	for (i=15, j=0; i<10 || j<8; i++, j++) {
- 		if (a[i] == '.') { j--; }
- 		else { temp[j] = a[i]; }
- 	}
- 	LastWaypoint.lat = CurrentWaypoint.lat;
- 	CurrentWaypoint.lat = atoi(temp);
-
- 	//current_direction = atof(msg->data.substr(29).c_str());
- 	// up to 6 decimal precision ~10cm, error ~2m
- 	ROS_INFO("Current status: ");
- 	cout << "lat: " << CurrentWaypoint.lat << endl;
- 	cout << "lon: " << CurrentWaypoint.lon << endl;
- 	//cout << current_direction << endl;
+	char a[100];
+	char nmea_type[7];
+	char temp[10];
+	int i = 0;
+	char lon_dir, lat_dir;
+	string str = msg->data;
+	cout.precision(13);
+	while (str[i] != '\0') { a[i] = str[i]; i++; }
+	for (i = 0; i < 6; i++)
+		nmea_type[i] = a[i];
+	string msg_type(nmea_type);
+	cout << msg_type << endl;
+	if (msg_type[4] == 'G' && msg_type[5] == 'A'){
+		msg_flag = true;
+		for (i = 18; i < 30; i++) 
+			temp [i-18] = a[i];
+		lon_dir = a[31];
+		//cout << lon_dir << endl;
+		if (lon_dir == 'N')
+			CurrentWaypoint.lon = atof(temp)/100 - off.lon;
+		else if (lon_dir == 'S')
+			CurrentWaypoint.lon = (-1)*(atof(temp)/100 - off.lon);
+		for (i = 33; i < 45; i++)
+			temp [i-33] = a[i];
+		lat_dir = a[47];
+		//cout << lat_dir << endl;
+		if (lat_dir == 'E')
+			CurrentWaypoint.lat = atof(temp)/100 - off.lat;
+		else if (lat_dir == 'W')
+			CurrentWaypoint.lat = (-1)*(atof(temp)/100 - off.lat);
+	}
+	else{
+		msg_flag = false;
+		cout << "Did not receive proper gps data" << endl;
+		}
+		
   return;
  }
+bool checkGoal (waypoint CurrentWaypoint, waypoint TargetWaypoint){
+  if (CurrentWaypoint.lon == TargetWaypoint.lon){
+    if (CurrentWaypoint.lat == TargetWaypoint.lat){
+      return true;
+    }
+    else
+      return false;
+  }
+  else
+    return false;
+}
 
+int NextMoveLogic (int prev_move, double distance, double angle){
+	int next_move; 
+	if (checkGoal) 
+		return 0;
+	else{
+		if (angle < 0.0) 
+			return 0.2; 
+		else if (angle > 0.0)
+			return -0.2;
+		else{
+			if (distance > 100)
+					return 0.5;
+			else if (distance < 100)
+				return 0.2; 
+			else 
+				cout << "Error in NextMoveLogic: " << distance << endl;}	
+}
+}
 geometry_msgs::Twist GetTwistMsg(int next_move) 
 {
 	geometry_msgs::Twist twist;
@@ -99,22 +137,105 @@ geometry_msgs::Twist GetTwistMsg(int next_move)
 	return twist;
 }
 
-int main (int argc, char **argv){
+double createDistance (void){
+	/*
+	Input Parameter: void
+	Output: distance in meteres from currentWaypoint to targetWaypoint
+	Purpose: calculates distance from target waypoints
+	Link:http://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude-python
+	*/
 
+	//requires CurrentWaypoint.lat, CurrentWaypoint.lon, TargetWaypoint.lat, TargetWaypoint.lon
+	double toRad = PI / 180;
+
+	double lat1 = CurrentWaypoint.lat * toRad;
+	double lon1 = CurrentWaypoint.lon * toRad;
+	double lat2 = TargetWaypoint.lat * toRad;
+	double lon2 = TargetWaypoint.lon * toRad;
+	//from http://www.movable-type.co.uk/scripts/latlong.html, Distance formula (using haversine)
+	double a = sin((lat2 - lat1) / 2)*sin((lat2 - lat1) / 2) + cos(lat1) * cos(lat2) * sin((lon2 - lon1) / 2)*sin((lon2 - lon1) / 2);
+	double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+	double d = 6371000 * c;
+	return d;
+}
+
+double createAngle(){
+	/*
+	Input Parameter:
+	1. direction of robot from North (0-359 degrees)
+	??
+	2. x cordinates of TargetWaypoint in metres
+	3. y cordinates of TargetWaypoint in metres
+	Output: the direction the robot needs to turn (-180 < theta < 180) 
+	Purpose: calculates angle of robot to target waypoint ((-180) to 180 degrees)
+	Author: Nick Wu
+	*/
+	double x = 1, y = 1; //x and y cordinates in metres, this needs to be calculated or passed in a paramaters
+
+	double theta = 0, angleWaypoint = 180; //theta: angle robot needs to turn, angleWaypoint goal angle from North
+	double r = sqrt(x*x + y*y); //r = distance from the robot to waypoint
+	double angleGoal = 180/PI * (acos(abs(y) / r)); //reference angle with respect to y-axis
+
+	if (x > 0 && y >= 0) //goal angle in quad 1
+		angleWaypoint = angleGoal;
+	else if (x >= 0 && y < 0) //goal angle in quad 4
+		angleWaypoint = (180 - angleGoal);
+	//checking special condition under quad 1 and 4
+	if (angleCompass > angleWaypoint + 180)
+		theta = 360 - angleCompass + angleWaypoint;
+
+	if (x < 0 && y <= 0) //goal angle in quad 3
+		angleWaypoint = angleGoal + 180;
+	else if (x < 0 && y > 0) //goal angle in quad 2 
+		angleWaypoint = 360 - angleGoal;
+	//checking special condition under quad 3 and 2 
+	if (angleCompass <= angleWaypoint - 180){
+		if (x < 0 && y <= 0) //checks for quad 3 
+			theta = angleGoal - angleCompass - 180;
+		else if (x < 0 && y > 0) //checks for quad 2
+			theta = -angleCompass - angleGoal;
+	}
+
+	//for any other condtions
+	if (theta == 0){
+		theta = angleWaypoint - angleCompass;
+	}
+	return theta;
+}
+int main (int argc, char **argv){
+  double gps_distance,gps_angle = 0.0;
   ros::init(argc, argv, AI_NODE_NAME); //initialize access point to communicate
   ros::NodeHandle nh; //create handle to this process node, NodeHandle is main access point to communication with ROS system. First one intializes node
   ros::Subscriber gps_Sub = nh.subscribe("GPS_USB_DATA", 20, gpsSubHandle);
-  ros::Publisher car_pub = nh.advertise<geometry_msgs::Twist>(PUB_TOPIC, 100);	
+  ros::Publisher car_pub = nh.advertise<geometry_msgs::Twist>(PUB_TOPIC, 100);
+  ros::Publisher gps_pub_d = nh.advertise<std_msgs::String>("GPS_DISTANCE",50);
+  ros::Publisher gps_pub_a = nh.advertise<std_msgs::String>("GPS_ANGLE",50);
 
-  ros::Rate loop_rate(10); //10hz loop rate
-
-
+  ros::Rate loop_rate(5); //10hz loop rate
+/*  while (!msg_flag){
+		cout << "Waiting for GPS Fix" << endl;	
+		loop_rate.sleep();
+	}
+  startGps();*/
   while (ros::ok()){
-    	  ROS_INFO ("Position:");
-	   cout << "Current longitude: " << CurrentWaypoint.lon << " Current latitude: " << CurrentWaypoint.lat << endl;
-  		   
-	  twist_msg = GetTwistMsg(next_movement);
+
+	  if (msg_flag){
+	    	  ROS_INFO ("Position:");
+		  cout << "Current longitude: " << CurrentWaypoint.lon << " Current latitude: " << CurrentWaypoint.lat << endl;
+	}
+	  else{
+		ROS_INFO("No Fix:"); 
+  	}
+	   //twist out 
+	  gps_distance = createDistance();
+	  gps_pub_d.publish(gps_distance); //d out 
+	  gps_angle = createAngle();
+	  gps_pub_a.publish(gps_angle); //a out 
+
+	  //next_move = NextMoveLogic(prev_move,gps_distance,gps_angle);
+	  twist_msg = GetTwistMsg(next_move);
 	  car_pub.publish(twist_msg);
+
 	  ros::spinOnce(); //ros spin is to ensure that ros threading does not leave suscribes un processed
 	  loop_rate.sleep();
 }
