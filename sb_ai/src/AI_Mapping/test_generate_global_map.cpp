e11bdf79416697c4e55aceef4a660d20fac882db
Justin Liang <justin.j.w.liang@gmail.com>
2015-06-05 18:53:27 +0200

    AI_Mapping: Fixed waypoint subscriber callback.

--- sb_ai/src/AI_Mapping/test_generate_global_map.cpp
+++ sb_ai/src/AI_Mapping/test_generate_global_map.cpp
@@ -11,70 +11,77 @@ float MAP_RESOLUTION = 0.5;
 // Make this global so we can unsubscribe after processing and storing the gpsOrigin to the global variable
 ros::Subscriber gpsOriginSubscriber;
 
-void gpsOriginSubscriberCallback(const sb_msgs::Waypoint::ConstPtr& gpsOriginMsg);
+void gpsOriginSubscriberCallback(
+		const sb_msgs::Waypoint::ConstPtr& gpsOriginMsg);
 
 int main(int argc, char **argv) {
 
-  ros::init(argc, argv, "test_generate_global_map");
+	ros::init(argc, argv, "test_generate_global_map");
 
-  ros::Time::init();
+	ros::Time::init();
 
-  ros::Rate loopRate(5); //10hz loop rate
+	ros::Rate loopRate(5); //10hz loop rate
 
-  while (ros::ok()){
+	while (ros::ok()) {
 
-    ros::NodeHandle n;
+		ros::NodeHandle n;
 
-    // We subscribe to this to get the GPS origin, this will run once and then the callback will close the subscriber
-    gpsOriginSubscriber = n.subscribe(WAYPOINT_TOPIC, 1000, &gpsOriginSubscriberCallback);
+		// We subscribe to this to get the GPS origin, this will run once and then the callback will close the subscriber
+		gpsOriginSubscriber = n.subscribe(WAYPOINT_TOPIC, 1000,
+				&gpsOriginSubscriberCallback);
 
-    GenerateGlobalMap generateGlobalMap(GPS_ORIGIN_GLOBAL_MSG, COURSE_WIDTH, COURSE_HEIGHT, MAP_RESOLUTION);
+		GenerateGlobalMap generateGlobalMap(GPS_ORIGIN_GLOBAL_MSG, COURSE_WIDTH,
+				COURSE_HEIGHT, MAP_RESOLUTION);
 
-    generateGlobalMap.testDoSomething();
+		generateGlobalMap.testDoSomething();
 
-    //// Create Test Local Map
-    nav_msgs::OccupancyGrid localMap;
-    localMap.info.width = 4;
-    localMap.info.height = 4;
-    localMap.data.assign(16, 0);
-    localMap.data[5] = 1;
-    ROS_INFO("YOLO");
+		//// Create Test Local Map
+		nav_msgs::OccupancyGrid localMap;
+		localMap.info.width = 4;
+		localMap.info.height = 4;
+		localMap.data.assign(16, 0);
+		localMap.data[5] = 1;
+		ROS_INFO("YOLO");
 
-    printf("Local Map:\n");
-    for(int i = 0; i < localMap.data.size(); i++) {
+		printf("Local Map:\n");
+		for (int i = 0; i < localMap.data.size(); i++) {
 
-      if((i+1) % localMap.info.width == 0) {
+			if ((i + 1) % localMap.info.width == 0) {
 
-        printf("%d ", localMap.data[i]);
-        printf("\n");
+				printf("%d ", localMap.data[i]);
+				printf("\n");
 
-      } else {
+			} else {
 
-        printf("%d ", localMap.data[i]);
+				printf("%d ", localMap.data[i]);
 
-      }
+			}
 
-    }
+		}
 
-    // Testing the subscribers in the class
-    ros::Publisher localMapPublisher = n.advertise<nav_msgs::OccupancyGrid>(VISION_TOPIC, 1000);
-    ros::Publisher waypointPublisher = n.advertise<sb_msgs::Waypoint>(WAYPOINT_TOPIC, 1000);
-    ros::Publisher gpsInfoPublisher = n.advertise<sb_msgs::Gps_info>(GPS_INFO_TOPIC, 1000);
-    
-    ros::spin(); //ros spin is to ensure that ros threading does not leave subscribes un processed
+		// Testing the subscribers in the class
+		ros::Publisher localMapPublisher = n.advertise < nav_msgs::OccupancyGrid
+				> (VISION_TOPIC, 1000);
+		ros::Publisher waypointPublisher = n.advertise < sb_msgs::Waypoint
+				> (WAYPOINT_TOPIC, 1000);
+		ros::Publisher gpsInfoPublisher = n.advertise < sb_msgs::Gps_info
+				> (GPS_INFO_TOPIC, 1000);
 
-    loopRate.sleep();
+		ros::spin(); //ros spin is to ensure that ros threading does not leave subscribes un processed
 
-  }
+		loopRate.sleep();
 
-  return 0;
+	}
+
+	return 0;
 
 }
 
-void gpsOriginSubscriberCallback(const sb_msgs::Waypoint::ConstPtr& gpsOriginMsg) {
+void gpsOriginSubscriberCallback(
+		const sb_msgs::Waypoint::ConstPtr& gpsOriginMsg) {
 
-    GPS_ORIGIN_GLOBAL_MSG.lon = gpsOriginMsg->lon;
-    GPS_ORIGIN_GLOBAL_MSG.lat = gpsOriginMsg->lat;
-    gpsOriginSubscriber.shutdown();
+	GPS_ORIGIN_GLOBAL_MSG.lon = gpsOriginMsg->lon;
+	GPS_ORIGIN_GLOBAL_MSG.lat = gpsOriginMsg->lat;
+	gpsOriginSubscriber.shutdown();
 
 }
